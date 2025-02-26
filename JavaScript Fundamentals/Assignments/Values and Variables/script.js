939f4ac8cafaa102ecbb6139468f52678ba30c11
HeoKhoDai <baokhanh20802@gmail.com>
2025-02-26 20:58:44 +0700

    value and variables

---
+++ JavaScript Fundamentals/Assignments/Values and Variables/script.js
@@ -0,0 +1,8 @@
+////////////////////////////////////
+// Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+// Log their values to the console.
+let country = "VietNam";
+let continent = "SEA";
+let population = "100M";
+
+console.log(country, continent, population);
--- JavaScript Fundamentals/script.js
+++ JavaScript Fundamentals/script.js
@@ -2,4 +2,13 @@
 let js = "amazing";
 console.log(40 + 8 + 1 + 5);
 
+////////////////////////////////////
+// Values and Variables
+console.log("Jonas");
+console.log(23);
 
+let firstName = "Piu";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
