da878cec2bca3eaa800007f16800ac3a85ff8c31
HeoKhoDai <baokhanh20802@gmail.com>
2025-02-26 22:17:17 +0700

    basic operators

--- JavaScript Fundamentals/script.js
+++ JavaScript Fundamentals/script.js
@@ -7,7 +7,7 @@ console.log(40 + 8 + 1 + 5);
 console.log("Piu");
 console.log(23);
 
-let firstName = "Piu";
+// let firstName = "Piu";
 
 console.log(firstName);
 console.log(firstName);
@@ -21,7 +21,7 @@ console.log(javascriptIsFun);
 // console.log(typeof true);
 console.log(typeof javascriptIsFun);
 // console.log(typeof 23);
-// console.log(typeof 'Jonas');
+// console.log(typeof 'Piu');
 
 javascriptIsFun = 'YES!';
 console.log(typeof javascriptIsFun);
@@ -34,3 +34,31 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
+
+////////////////////////////////////
+// Basic Operators
+// Math Operators
+const now = 2025
+const ageKhanh = now - 2002;
+const agePiu = now - 2005;
+console.log(ageKhanh);
+
+console.log(ageKhanh * 3, ageKhanh / 2, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3  = 2 * 2 * 2
+const firstName = "Khanh";
+const lastName = "Nguyen";
+console.log(firstName + " " + lastName);
+
+//Assignment Operators
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+console.log(x);
+
+// Comparison math
+console.log(ageKhanh > agePiu); // >, <, >=, <=
+
+const isFullAge = agePiu >= 1;
+console.log(now - 2002 > now - 2005);
