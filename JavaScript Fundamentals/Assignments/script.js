e777d05cb63427f36484953e2a0d9c781b15b661
HeoKhoDai <baokhanh20802@gmail.com>
2025-02-26 21:56:34 +0700

    let, const and var

--- JavaScript Fundamentals/Assignments/script.js
+++ JavaScript Fundamentals/Assignments/script.js
@@ -19,3 +19,32 @@ console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
+
+////////////////////////////////////
+// let, const and var
+let age = 22;
+age = 23;
+
+const birthYear = 2002;
+// birthYear = 2003;
+// const job;
+
+var job = 'programmer';
+job = 'student'
+
+lastName = 'Nguyen';
+console.log(lastName);
+
+/*
+1. Set the value of language to the language spoken where you live (some countries have multiple languages, but just choose one).
+
+2. Think about which variables should be const variables (which values will never change, and which might change?). Then, change these variables to const.
+
+3. Try to change one of the changed variables now, and observe what happens.
+
+language = 'vietnamese';
+const country = 'Vietnam';
+const continent = 'Asia';
+const isIsland = false;
+isIsland = true;
+*/
