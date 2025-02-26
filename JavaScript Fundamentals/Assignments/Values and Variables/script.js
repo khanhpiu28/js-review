48c02fc2f1bd5ac02106339a6d0cfdbc8f86be94
HeoKhoDai <baokhanh20802@gmail.com>
2025-02-26 21:19:28 +0700

    data types

--- JavaScript Fundamentals/Assignments/Values and Variables/script.js
+++
@@ -1,8 +0,0 @@
-////////////////////////////////////
-// Declare variables called country, continent and population and assign their values according to your own country (population in millions).
-// Log their values to the console.
-let country = "VietNam";
-let continent = "SEA";
-let population = "100M";
-
-console.log(country, continent, population);
--- JavaScript Fundamentals/script.js
+++ JavaScript Fundamentals/script.js
@@ -4,7 +4,7 @@ console.log(40 + 8 + 1 + 5);
 
 ////////////////////////////////////
 // Values and Variables
-console.log("Jonas");
+console.log("Piu");
 console.log(23);
 
 let firstName = "Piu";
@@ -12,3 +12,25 @@ let firstName = "Piu";
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
+
+////////////////////////////////////
+// Data Types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
