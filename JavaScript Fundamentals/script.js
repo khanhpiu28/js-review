02a2d10d6c18c519ecb3c8204bb83e4e49a43427
HeoKhoDai <baokhanh20802@gmail.com>
2025-02-26 23:18:07 +0700

    type conversion & coersion

--- JavaScript Fundamentals/script.js
+++ JavaScript Fundamentals/script.js
@@ -134,3 +134,25 @@ if (markHigherBMI) {
 } else {
     console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
 }
+
+////////////////////////////////////
+// Type Conversion and Coercion
+
+// type conversion
+const inputYear = '2002';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Piu')); // NaN
+console.log(typeof NaN); // number
+
+console.log(String(23), 23);
+
+// type coercion
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3); // 10
+console.log('23' / '2');
+
+let n = '1' + 1; // '11'
+n = n - 1;
+console.log(n); //10
