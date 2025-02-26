86d8dff2f41ec64f9723aae4fd78b6b05a10ab62
HeoKhoDai <baokhanh20802@gmail.com>
2025-02-26 23:53:54 +0700

    logical operators

--- JavaScript Fundamentals/script.js
+++ JavaScript Fundamentals/script.js
@@ -204,3 +204,27 @@ if (favorite === 23) {
 }
 
 if (favorite !== 23) console.log('Why not 23?');
+
+////////////////////////////////////
+// Logical Operators
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+// if (hasDriversLicense && hasGoodVision) {
+//   console.log('You is able to drive!');
+// } else {
+//   console.log('Someone else should drive...');
+// }
+
+const isTired = false; // C
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+    console.log('You is able to drive!');
+} else {
+    console.log('Someone else should drive...');
+}
