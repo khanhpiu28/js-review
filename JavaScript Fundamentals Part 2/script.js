554205689ed94b875d40ef973490b91ba5bfb63d
HeoKhoDai <baokhanh20802@gmail.com>
2025-02-28 14:15:24 +0700

    docket vs bracket notation

--- JavaScript Fundamentals Part 2/script.js
+++ JavaScript Fundamentals Part 2/script.js
@@ -160,10 +160,10 @@ friends[2] = 'Jay';
 console.log(friends);
 // friends = ['Bob', 'Alice']
 
-const firstName = 'Jonas';
-const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
-console.log(jonas);
-console.log(jonas.length);
+// const firstName = 'Jonas';
+// const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+// console.log(jonas);
+// console.log(jonas.length);
 
 // Exercise
 const calcAge = function (birthYeah) {
@@ -259,3 +259,35 @@ const j = {
     job: 'teacher',
     friends: ['Michael', 'Peter', 'Steven']
 };
+
+///////////////////////////////////////
+// Dot vs. Bracket Notation
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+};
+console.log(jonas);
+
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(jonas['last' + nameKey]);
+
+// console.log(jonas.'last' + nameKey)
+
+const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends');
+
+if (jonas[interestedIn]) {
+    console.log(jonas[interestedIn]);
+} else {
+    console.log('Wrong request! Choose between firstName, lastName, age, job, and friends');
+}
+
+jonas.location = 'Portugal';
+jonas['twitter'] = '@jonasschmedtman';
+console.log(jonas);
