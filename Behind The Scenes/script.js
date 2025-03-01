ee45b77f3d412206ca8e0d89bb226526dd87fe80
HeoKhoDai <baokhanh20802@gmail.com>
2025-03-01 20:38:26 +0700

    object references in practice

--- Behind The Scenes/script.js
+++ Behind The Scenes/script.js
@@ -132,3 +132,50 @@ var addArrow = (a, b) => {
   return a + b;
 };
 addArrow(2, 5, 8);
+
+///////////////////////////////////////
+// Object References in Practice (Shallow vs. Deep Copies)
+
+const jessica1 = {
+  firstName: "Jessica",
+  lastName: "Williams",
+  age: 27,
+};
+
+function marryPerson(originalPerson, newLastName) {
+  originalPerson.lastName = newLastName;
+  return originalPerson;
+}
+
+const marriedJessica = marryPerson(jessica1, "Davis");
+
+// const marriedJessica = jessica1;
+// marriedJessica.lastName = 'Davis';
+
+console.log("Before:", jessica1);
+console.log("After:", marriedJessica);
+
+const jessica = {
+  firstName: "Jessica",
+  lastName: "Williams",
+  age: 27,
+  family: ["Alice", "Bob"],
+};
+
+// Shallow copy
+const jessicaCopy = { ...jessica };
+jessicaCopy.lastName = "Davis";
+
+// jessicaCopy.family.push('Mary');
+// jessicaCopy.family.push('John');
+
+// console.log('Before:', jessica);
+// console.log('After:', jessicaCopy);
+
+// Deep copy/clone
+const jessicaClone = structuredClone(jessica);
+jessicaClone.family.push("Mary");
+jessicaClone.family.push("John");
+
+console.log("Original:", jessica);
+console.log("Clone:", jessicaClone);
