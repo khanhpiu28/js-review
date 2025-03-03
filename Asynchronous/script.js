50c9560ce4d3ea3e8f367f87f3c9633fe46c4a69
HeoKhoDai <baokhanh20802@gmail.com>
2025-03-03 23:21:39 +0700

    chaining, handling rejected promises

--- Asynchronous/script.js
+++ Asynchronous/script.js
@@ -59,17 +59,36 @@ const renderCountry = function (data, className = "") {
 
 ///////////////////////////////////////
 // Consuming Promises
+// Chaining Promises
+// Handling Rejected Promises
 
 const getCountryData = function (country) {
-  fetch(`https://restcountries.com/v2/name/${country}`)
-    .then(function (response) {
-      console.log(response);
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
+  // Country 1
+  getJSON(`https://restcountries.com/v2/name/${country}`, "Country not found")
+    .then((data) => {
       renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) throw new Error("No neighbour found!");
+
+      // Country 2
+      return getJSON(
+        `https://restcountries.eu/rest/v2/alpha/${neighbour}`,
+        "Country not found"
+      );
+    })
+
+    .then((data) => renderCountry(data, "neighbour"))
+    .catch((err) => {
+      console.error(`${err} 💥💥💥`);
+      renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
     });
 };
 
+btn.addEventListener("click", function () {
+  getCountryData("portugal");
+});
 getCountryData("portugal");
